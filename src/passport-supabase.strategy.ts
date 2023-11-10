512f12c46820047ae41db1838bb332043e0ff684
Nicolas Diaz <niko476678@gmail.com>
2023-11-10 12:16:08 -0300

    Changes to make it works on Nest

--- src/passport-supabase.strategy.ts
+++ src/passport-supabase.strategy.ts
@@ -34,8 +34,16 @@ export class SupabaseAuthStrategy extends Strategy {
     this.extractor = options.extractor;
   }
 
-  async validate(payload: SupabaseAuthUser): Promise<SupabaseAuthUser> {
-    return payload;
+  async validate(payload: SupabaseAuthUser): Promise<SupabaseAuthUser | null> {
+    if (!!payload) {
+      this.success(payload, {});
+
+      return payload;
+    }
+
+    this.fail(UNAUTHORIZED, 401);
+
+    return null;
   }
 
   authenticate(req: Request): void {
@@ -46,21 +54,12 @@ export class SupabaseAuthStrategy extends Strategy {
       return;
     }
 
-    this.supabase.auth.api
+    this.supabase.auth
       .getUser(idToken)
-      .then((res) => this.validateSupabaseResponse(res))
+      .then(({ data: { user }}) => this.validate(user))
       .catch((err) => {
         this.fail(err.message, 401);
       });
   }
 
-  private async validateSupabaseResponse({ data }: any) {
-    const result = await this.validate(data);
-    if (result) {
-      this.success(result, {});
-      return;
-    }
-    this.fail(UNAUTHORIZED, 401);
-    return;
-  }
 }
